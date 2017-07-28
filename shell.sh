1dd3c23b292741dae62b2fb60e7a383a5ff24622
Laura McRae <lmcrae@newrelic.com>
2017-07-28 10:56:29 -0700

    used gitapi to make pull request

--- shell.sh
+++ shell.sh
@@ -11,3 +11,4 @@ echo "sdfsfdheeeeeeey" > test.txt
 git add --all
 git commit -m "automated push"
 git push -u origin t
+curl -u mcraeklaura:lionpath11 -X POST -H 'Content-type: application/json' --data '{"title":"Amazing new feature","body":"Please pull this in!","head":"mcraeklaura:t","base":"master"}' https://api.github.com/repos/mcraeklaura/testing/pulls
