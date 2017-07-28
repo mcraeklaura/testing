21ad6ec4fe4c73747ae805db047dfc1f82ae600b
Laura McRae <lmcrae@newrelic.com>
2017-07-28 13:42:35 -0700

    f

--- shell.sh
+++ shell.sh
@@ -4,14 +4,17 @@ HAS_PULL=0
 if [ !`git branch -a | egrep "^[[:space:]]+${BRANCH}$"` ]
 then
    # If the branch doesn't exist, make it again.
+   echo "Making new branch"
    git checkout -b ${BRANCH}
    HAS_PULL=1
 else
+   echo "Checking into the branch"
    git checkout ${BRANCH}
 fi
 
 # Insert changes here
 echo $(date) > test.txt
+echo "Changing stuffs"
 
 git add --all
 git commit -m "Making changes to CPU/MEM data"
@@ -19,6 +22,7 @@ git push -u origin ${BRANCH}
 
 if [ HAS_PULL ]
 then
+   echo "Making a new pull request"
    # Make new pull request if there isn't already one
    curl -u mcraeklaura:lionpath11 -X POST -H 'Content-type: application/json' \
    --data '{"title":"BOT Make changes to FILES","body":"Please pull this in!","head":"mcraeklaura:'+$BRANCH+'","base":"master"}' \
