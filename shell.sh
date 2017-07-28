00f1da229ce1fb07e58bb8dc25f90d3cec560449
Laura McRae <lmcrae@newrelic.com>
2017-07-28 09:32:33 -0700

    actually going to save it this time lol

--- shell.sh
+++ shell.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo $1 > test.txt
+if [ `git branch --list t` ]
+then
+   git branch -d t
+   git push origin --delete t
+fi
+git branch t
+git checkout t
+echo "heeeeeeey" > test.txt
+git add --all
+git commit -m "automated push"
+git push
